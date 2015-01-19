ea4ce6455b78eae4982b779e6df64252bf436da9
Nicholas Klunder <nklunder@gmail.com>
2015-01-19 07:49:25 -0800

    add browser-sync module to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,33 @@ var rename = require('gulp-rename');
 var sass   = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
+var sync   = require('browser-sync');
+var reload = sync.reload;
+
+
 gulp.task('scripts', function () {
   return gulp.src('js/*.js')
     .pipe(concat('app.js'))
       .pipe(rename({suffix: '.min'}))
-      .pipe(uglify('last 2 versions', '> 5% in US'))
+      .pipe(uglify())
       .pipe(gulp.dest('build/js/'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src('sass/app.scss')
+  return gulp.src('sass/**/*.scss')
     .pipe(rename({suffix: '.min'}))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(prefix())
-    .pipe(gulp.dest('build/css/'));
+    .pipe(gulp.dest('build/css/'))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['scripts', 'styles']);
+gulp.task('sync', function () {
+  sync({
+    server: { baseDir: "./" }
+  });
+});
+
+gulp.task('default', ['scripts', 'styles', 'sync'], function () {
+  gulp.watch('sass/*.scss', ['styles']);
+});
